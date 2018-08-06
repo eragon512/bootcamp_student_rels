10b3461fa837fcc49567e4a56db60f2b8433d0f0
Anirud Samala <anirud.samala+github@gmail.com>
2018-08-06 09:52:41 +0530

    added file input util function

---
+++ fileInput.js
@@ -0,0 +1,23 @@
+const Student = require('./student');
+const fs = require('fs');
+
+function getStudentsFromFile(filename) {
+  let fileData = fs.readFileSync(filename,'utf8');
+  fileData = fileData.split('\n').slice(0,-1);
+
+  let students = [];
+  for(let line of fileData) {
+    console.log(line);
+    let tmpList = line.split(' ');
+    let tmpStudentName = tmpList.slice(0,1);
+    let tmpStudentMarks = tmpList.slice(1).map(s => parseInt(s));
+    let tmpStudent = new Student(tmpStudentName,tmpStudentMarks);
+    students = [].concat(students,tmpStudent);
+  }
+  return students;
+
+}
+
+module.exports = {
+  getStudentsFromFile: getStudentsFromFile
+};
